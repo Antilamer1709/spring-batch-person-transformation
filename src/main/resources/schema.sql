7dbd56ceced234c22612f12c0c8ffd2bd5306232
vslobo <antilamer1709@gmail.com>
2020-09-02 21:07:28 +0300

    batch classes working schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,15 +7,12 @@ CREATE TABLE PUBLIC.people  (
 );
 
 
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
-INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat', 'Dog');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat1', 'Dog1');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat2', 'Dog2');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat3', 'Dog3');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat4', 'Dog4');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat5', 'Dog5');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat6', 'Dog6');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat7', 'Dog7');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat8', 'Dog8');
+INSERT INTO PUBLIC.people (first_name, last_name) VALUES ('Cat9', 'Dog9');
